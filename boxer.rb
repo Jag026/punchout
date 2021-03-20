877d4699b54f4534882b49d124d733c087fcde60
Drew <DGriffin@appacademy.io>
2021-03-20 01:47:48 -0500

    boxer class and gameboard

---
+++ boxer.rb
@@ -0,0 +1,58 @@
+class Player
+    def initialize(name)
+        @name = name
+        @health = 12
+        @moves_arr = ["high", "mid", "low"]
+    end
+
+    def name
+        @name
+    end
+
+    def health
+        @health
+    end
+
+    def lose_health(num)
+        @health = @health - num
+    end
+
+    def attack_amt
+        return rand(5)
+    end
+
+    def throw_punch
+        num_arr = (1..@moves_arr.length).to_a.map { |num| num.to_s }
+        player_move = gets.chomp.to_s
+        if player_move.to_i <= @moves_arr.length && num_arr.any?(player_move)
+            player_move = player_move.to_i - 1
+            return @moves_arr[player_move]
+        else
+            p "Invalid Move"
+            self.throw_punch
+        end
+    end
+
+    def defend_punch
+        num_arr = (1..@moves_arr.length).to_a.map { |num| num.to_s }
+        player_move = gets.chomp.to_s
+        if player_move.to_i <= @moves_arr.length && num_arr.any?(player_move)
+            player_move = player_move.to_i - 1
+            return @moves_arr[player_move]
+        else
+            p "Invalid Move"
+            self.defend_punch
+        end
+    end
+
+    def auto_punch
+       random_num = rand(@moves_arr.length)
+       return @moves_arr[random_num]
+    end
+
+    def auto_defend
+       random_num = rand(@moves_arr.length)
+       return @moves_arr[random_num]
+    end
+end
+
---
+++ punchout.rb
@@ -0,0 +1,33 @@
+require_relative "boxer"
+
+
+p1 = Player.new("bilbo")
+p2 = Player.new("computer")
+
+while p1.health > 0 && p2.health > 0
+    p1_attack = p1.throw_punch
+    p2_defense = p2.auto_defend
+    
+    if p1_attack != p2_defense
+        p2.lose_health(p1.attack_amt)
+    end
+
+    p2_attack = p2.auto_punch
+    p1_defense = p1.defend_punch
+    
+    if p1_attack != p2_defense
+        p1.lose_health(p2.attack_amt)
+    end
+
+    puts
+    puts "Player 1 Health"
+    puts p1.health
+    puts 
+    puts p2.health
+    puts "Player 2 Health"
+    puts
+
+    p p1.name + " Is the loser" if p1.health <= 0
+    p p2.name + " Is the loser" if p2.health <= 0
+end
+
